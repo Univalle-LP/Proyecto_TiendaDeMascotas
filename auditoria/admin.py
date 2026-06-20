3aff09e466f6f93641bdc13a1c748cf4f9a95744
anachurata0203 <csa2027553@est.univalle.edu>
2026-06-20 01:50:46 -0400

    registrar eliminación de registros

---
+++ auditoria/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from .models import AuditLog
+
+@admin.register(AuditLog)
+class AuditLogAdmin(admin.ModelAdmin):
+    list_display = ('usuario', 'accion', 'entidad', 'fecha_hora')
+    list_filter = ('accion', 'entidad', 'fecha_hora')
+    search_fields = ('usuario__nombre', 'entidad')
+    readonly_fields = ('usuario', 'fecha_hora', 'accion', 'entidad', 'descripcion')
+    
+    def has_add_permission(self, request):
+        return False
+    
+    def has_delete_permission(self, request, obj=None):
+        return request.user.is_superuser
+    
+    def has_change_permission(self, request, obj=None):
+        return False
